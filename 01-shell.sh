8bd129db7dedaffbf2e0de603dd8117854fd7a5b
windevops9scm <windevops9@gmail.com>
2021-07-19 18:27:36 +0530

    created directory roboshop

--- 01-shell.sh
+++ 01-shell.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 echo "hello user"
-read user
-echo welcome $user
+read -r user
+echo "welcome $user"
 
 ls -lrt
 
 #variables
 #functions
 #regular expressions
+echo this is \n\tend
+
+echo -e "this is \n\tend"
+
+echo -e "\e[31mthis is \n\tend"
+
